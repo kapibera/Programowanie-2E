43826f150af49d7edd79b2ca56c816967bdbc310
olek <olek.h.kielce@gmail.com>
2022-09-21 09:53:19 +0200

    .

--- first project/first project/first project.cpp
+++ first project/first project/first project.cpp
@@ -4,5 +4,12 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+    std::cout << -56;
+    std::cout << "\n";
+    std::cout << "56";
+    std::cout << "\n";
+    std::cout << +15.5;
+    std::cout << "\n";
 }
 
