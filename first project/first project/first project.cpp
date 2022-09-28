0191c59b5fc1e0e41bf2a03d80a47fefc258c4f6
olek <olek.h.kielce@gmail.com>
2022-09-28 09:52:59 +0200

    .

--- first project/first project/first project.cpp
+++ first project/first project/first project.cpp
@@ -11,5 +11,11 @@ int main()
     std::cout << "\n";
     std::cout << +15.5;
     std::cout << "\n";
+
+
+    std::cout << "Podaj liczbe:\n";
+    int numberFromUser;
+    std::cin >> numberFromUser;
+    std::cout << "Podano:" << numberFromUser << "\n";
 }
 
