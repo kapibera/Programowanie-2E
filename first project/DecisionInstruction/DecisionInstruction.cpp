1e9f1c96e325b9e0609e195608b18e9c19e75dab
olek <olek.h.kielce@gmail.com>
2022-10-12 09:51:17 +0200

    .

---
+++ first project/DecisionInstruction/DecisionInstruction.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+/*Napisz funkcje, która wczyta dwie liczby i wykona dzielenie na nich*/
+
+void task1()
+{
+
+	int firstNumber, secondNumber;
+
+	std::cout << "Podaj pierwsza liczbe\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Podaj druga liczbe\n";
+	std::cin >> secondNumber;
+
+	int quotient;
+
+	quotient = firstNumber / secondNumber;
+
+	std::cout << "Iloraz" << quotient << "\n";
+}
+
+int main()
+{
+	task1()
+}
+
