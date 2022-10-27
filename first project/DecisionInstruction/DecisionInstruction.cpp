fee569c50002805dc7e7ee362ff182ad446a5b58
olek <olek.h.kielce@gmail.com>
2022-10-27 15:08:44 +0200

    .

--- first project/DecisionInstruction/DecisionInstruction.cpp
+++ first project/DecisionInstruction/DecisionInstruction.cpp
@@ -6,7 +6,6 @@
 /*Napisz funkcje, która wczyta dwie liczby i wykona dzielenie na nich*/
 void task1()
 {
-
 	int firstNumber, secondNumber;
 
 	std::cout << "Podaj pierwsza liczbe\n";
@@ -18,28 +17,30 @@ void task1()
 	//iloaz
 	int quotient;
 
-	/*if (secondNumber != 0);
-		quotient = firstNumber / secondNumber;
+	if (secondNumber != 0);
+	quotient = firstNumber / secondNumber;
 
 	if (secondNumber != 0)
 		std::cout << "Iloraz: " << quotient << "\n";
 
 	if (secondNumber == 0);
-	std::cout << "Dzielenie przez zero!!!\n";*/
+	std::cout << "Dzielenie przez zero!!!\n";
+
 
 	if (secondNumber != 0);
-	{
+	{	
 		quotient = firstNumber / secondNumber;
 		std::cout << "Iloraz: " << quotient << "\n";
 	}
-	else
+	
+	
 	{
 		std::cout << "Dzielenie przez zero!!!\n";
 	}
 
 	std::cout << "Koniec programu\n";
-
 }
+
 
 /*Napisz funkcje, która wczyta liczbê i wyœwietli informacje czy jest ona dodatnia czy nie*/
 void task2()
@@ -49,7 +50,23 @@ void task2()
 	std::cout << "Podaj pierwsza liczbe\n";
 	std::cin >> numberToCheck;
 
+	if (numberToCheck > 0)
+		std::cout << "Liczba wiêksza od zera\n";
+	else
+		std::cout << "Liczba mniejsza od zera\n";
 
+}
+
+/*Napisz funkcje, która wczyta liczbê i wyœwietli informacjê czy jest ona w zakresie <10, 25>*/
+void task3()
+{
+	int numberToCheck;
+
+	std::cout << "Podaj pierwsza liczbe\n";
+	std::cin >> numberToCheck;
+
+	std::cout << "Liczba jest w zakresie\n";
+	std::cout << "Liczba nie jest w zakresie\n";
 }
 
 
@@ -57,5 +74,6 @@ void task2()
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	task3()
 }
