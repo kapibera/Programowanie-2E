aa954623ec6be82646993554e50ad1fabfecc0dc
olek <olek.h.kielce@gmail.com>
2022-11-08 09:01:06 +0100

    .

--- first project/DecisionInstruction/DecisionInstruction.cpp
+++ first project/DecisionInstruction/DecisionInstruction.cpp
@@ -6,7 +6,7 @@
 
 void task4()
 {
-	int numberToCheck, checkNumber ;
+	int numberToCheck, checkNumber;
 
 	std::cout << "Podaj liczbê\n";
 	std::cin >> numberToCheck;
@@ -16,13 +16,45 @@ void task4()
 	if (checkNumber > 0)
 		std::cout << "liczba nie jest parzysta";
 	else
-			std::cout << "liczba jest parzysta";
+		std::cout << "liczba jest parzysta";
 }
 
 void task6()
 {
 
+	int firstNumber, secondNumber;
 
+	std::cout << "Podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
+
+	if (firstNumber > secondNumber)
+		std::cout << firstNumber << "jest wiêksza";
+	else
+		std::cout << secondNumber << "jest wieksza\n";
+}
+
+void task7()
+{
+	int firstNumber, secondNumber, thirdNumber;
+
+	std::cout << "Podaj pierwsza liczbe\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Podaj druga liczbe\n";
+	std::cin >> secondNumber;
+
+	std::cout << "Podaj trzecia liczbe\n";
+	std::cin >> thirdNumber;
+
+	if (secondNumber < firstNumber > thirdNumber)
+		std::cout << firstNumber << "jest wieksza";
+	else if (firstNumber < secondNumber > thirdNumber)
+		std::cout << secondNumber << "jest wieksza";
+	else if (firstNumber < thirdNumber > secondNumber)
+		std::cout << thirdNumber << "jest wieksza";
 
 }
 
@@ -32,7 +64,8 @@ void task6()
 
 int main()
 {
-	task4();
-	task6();
+	//task4();
+	//task6();
+	task7();
 
 }
